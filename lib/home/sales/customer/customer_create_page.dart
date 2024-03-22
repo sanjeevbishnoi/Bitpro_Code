6b6d66c3a93cf11915466ccb99ca3747dd840925
Ismail Ibrahim <82366222+ismailme33@users.noreply.github.com>
2024-03-22 22:30:52 +0300

    Update customer_create_page.dart

--- lib/home/sales/customer/customer_create_page.dart
+++ lib/home/sales/customer/customer_create_page.dart
@@ -309,7 +309,7 @@ class _CustomerCreateEditPageState extends State<CustomerCreateEditPage> {
                                           }),
                                         ),
                                         const SizedBox(
-                                          height: 50,
+                                          height: 30,
                                         ),
                                         BTextField(
                                           label: 'Phone 01',
